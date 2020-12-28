a4e82d36ae44f2258ddfe195e8bb5255a8b1ddd1
livvyvi3 <machinguravongai@gmail.com>
2020-12-28 18:01:15 +0200

    Add project files.

---
+++ StopWatch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            for (var i = 0; i < 2; i++)
+            {
+                stopwatch.Start();
+
+                Thread.Sleep(100);
+
+                stopwatch.Stop();
+                Console.WriteLine("Duration: " + stopwatch.GetInterval().ToString());
+                Console.WriteLine("Press Enter to Reset");
+                Console.ReadLine();
+            }
+        }
+    }
+    
+}
---
+++ StopWatch/Stopwatch.cs
@@ -0,0 +1,47 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    public class Stopwatch
+    {
+        private DateTime startTime;
+        private DateTime endTime;
+        private bool isRunning = false;
+
+        // class method to start 
+        public void Start ()
+        {
+            if (isRunning)
+            {
+                // gives the exception if code is running already
+                throw new InvalidOperationException("Stop watch is already running");
+            }
+            else
+            {
+                startTime = DateTime.Now;
+                isRunning = true;
+  
+            }
+        }
+
+        public void Stop()
+        {
+            if(isRunning)
+            {
+                endTime = DateTime.Now;
+                isRunning = false;
+            }
+
+        }
+
+        public TimeSpan GetInterval()
+        {
+            return endTime - startTime;
+         
+        }
+    }
+}
